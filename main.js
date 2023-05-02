75cd7b307bfccd3ca159a3c35529f8c2b1a7505c
Tanmay Aseri <87698651+Tanmay-Aseri@users.noreply.github.com>
2023-05-02 16:16:53 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 img = "";
+status = "";
 
 function setup(){
     canvas = createCanvas(640, 420);
     canvas.center();
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
 }
 
 function draw(){
     image(img, 0, 0, 640, 420);
     fill("#FF0000");
-    text("Dog", 45, 75);noFill();
+    text("Dog", 45, 75);
+    noFill();
     stroke("#FF0000");
     rect(30, 60, 450, 300);
     fill("#FF0000");
@@ -20,4 +24,19 @@ function draw(){
 
 function preload(){
      img = loadImage("dog_cat.jpg");
+}
+
+function modelLoaded(){
+    console.log("Model Loaded !");
+    status = true;
+    objectDetector.detect(img, gotResult);
+
+}
+
+
+function gotResult(error, results){
+    if (error) {
+       console.log(error);         
+    }
+    console.log(results);
 }
