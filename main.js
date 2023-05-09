c8e4c4859bde925a423cbab0f918d447e56aa7e4
Tanmay Aseri <87698651+Tanmay-Aseri@users.noreply.github.com>
2023-05-09 16:28:49 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,26 +1,33 @@
-img = "";
+
 status = "";
 object = [];
 
 function setup(){
-    canvas = createCanvas(640, 420);
+    canvas = createCanvas(380, 380);
     canvas.center();
-    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
-    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    video = createCapture(VIDEO);
+    video.size(380, 380);
+    video.hide();
+    
 }
 
 function draw(){
-    image(img, 0, 0, 640, 420);
+    image(video, 0, 0, 380, 380);
     if(status != ""){
-
+        r = random(255);
+        g = random(255);
+        b = random(255);
+        objectDetector.detect(video, gotResult);
         for(i = 0; i < object.length; i++){
         document.getElementById("status").innerHTML = "Status : Object Detected";
+        document.getElementById("number_of_objects").innerHTML = "Number of Objects that are detected are :"+ object.length;
 
-        fill("#FF0000");
+
+        fill(r, g, b);
         percent = floor(object[i].confidence * 100);
-        text(object[i].label+""+percent+"%",object[i].x + 15, object[i].y + 15);
+        text(object[i].label+" "+percent+"% ",object[i].x + 15, object[i].y + 15);
         noFill();
-        stroke("#FF0000");
+        stroke(r, g, b);
         rect(object[i].x, object[i].y, object[i].width, object[i].height);
     }
 
@@ -34,7 +41,7 @@ function preload(){
 function modelLoaded(){
     console.log("Model Loaded !");
     status = true;
-    objectDetector.detect(img, gotResult);
+
 
 }
 
@@ -45,4 +52,9 @@ function gotResult(error, results){
     }
     console.log(results);
     object = results;
+}
+
+function start(){
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
 }
